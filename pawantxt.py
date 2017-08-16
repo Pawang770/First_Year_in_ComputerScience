65bde689e867483f84f22bc3706c6cad0db12aac
Pawang770 <30855183+Pawang770@users.noreply.github.com>
2017-08-16 10:56:27 +0530

    Add files via upload

---
+++ pawantxt.py
@@ -0,0 +1,23 @@
+a="hello world"
+b="Hello WoRld"
+c="HELLO WORLD"
+d="hello world"
+e=" Hello World "
+f="a.b.c.e"
+g="hai"
+h="-"
+i=("hello","world")
+print(a.find("world"))
+print(c.lower())
+print(d.upper())
+print(a.title())
+print(a.capitalize())
+print(c.swapcase())
+print(a.replace("hello","Heloo"))
+print(e.strip())
+print(e.lstrip())
+print(e.rstrip())
+print(f.split("."))
+print(h.join(i))
+print(a.startswith("welcome"))
+print(a.endswith("world"))
