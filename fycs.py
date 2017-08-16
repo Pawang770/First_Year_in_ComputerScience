6c12d023f6134a2ccf169dce31a5cb7dbbb47f4b
Pawang770 <30855183+Pawang770@users.noreply.github.com>
2017-08-16 10:59:41 +0530

    Coding for Python

---
+++ fycs.py
@@ -0,0 +1,21 @@
+a="hello world"
+b="Hello WoRld"
+c="HELLO WORLD"
+d="hello world"
+e=" Hello World "
+f="a.b.c.e"
+g="hai"
+print(a.find("world"))
+print(c.lower())
+print(d.upper())
+print(a.title())
+print(a.capitalize())
+print(c.swapcase())
+print(a.replace("hello","Heloo"))
+print(e.strip())
+print(e.lstrip())
+print(e.rstrip())
+print(f.split("."))
+print(a.join(g))
+print(a.startswith("welcome"))
+print(a.endswith("world"))
